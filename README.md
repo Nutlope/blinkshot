79ccb3ec8b2de3d6f8fd75700351c7eafbdd97bf
Hassan El Mghari <hassan4709@gmail.com>
2024-10-04 10:53:00 -0700

    edited footer

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 
 - [Llama 3.1 8B](https://ai.meta.com/blog/meta-llama-3-1/) from Meta for the LLM
 - [Llama 3.2 Vision](https://ai.meta.com/blog/meta-llama-3-1/) from Meta for the Vision model
-- [Together AI](https://dub.sh/together) for LLM inference
+- [Together AI](https://www.dub.sh/together-ai) for LLM inference
 - [Sandpack](https://sandpack.codesandbox.io/) for the code sandbox
 - [S3](https://aws.amazon.com/s3/) for image storage
 - Next.js app router with Tailwind
@@ -21,17 +21,21 @@
 ## Cloning & running
 
 1. Clone the repo: `git clone https://github.com/Nutlope/napkins`
-2. Create a `.env` file and add your [Together AI API key](https://dub.sh/llama3.2vision/?utm_source=example-app&utm_medium=napkins&utm_campaign=napkins-app-signup): `TOGETHER_API_KEY=`
+2. Create a `.env` file and add your [Together AI API key](https://www.dub.sh/together-ai): `TOGETHER_API_KEY=`
 3. Create an S3 bucket and add the credentials to your `.env` file. Follow [this guide](https://next-s3-upload.codingvalue.com/setup) to set them up. All required values are in the `.env.example` file.
 4. Run `npm install` and `npm run dev` to install dependencies and run locally
 
+# TODOs
+
+- [ ] Add observability with Helicone
+- [ ] Add rate limiting by IP w/ a credit system that resets every 24h
+- [ ] Clean up README, make demo vid, and post on X
+
 ## Future Tasks
 
-- [ ] Add links to the footer
+- [ ] Add a CTA to fork the code on GithUb
 - [ ] On hover, have a download button
-- [ ] Add a description of the appto the footer
-
 - [ ] Let people play around with resolutions
+- [ ] Let people play around with steps
+- [ ] Add a description of the app to the footer
 - [ ] Add themes
-
-# TODOs
--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
   return (
     <div className="flex h-full flex-col px-5">
       <header className="flex justify-center pt-6">
-        <a href="https://www.dub.sh/together" target="_blank">
+        <a href="https://www.dub.sh/together-ai" target="_blank">
           <Logo />
         </a>
       </header>
@@ -81,7 +81,7 @@ export default function Home() {
             </p>
           </div>
         ) : (
-          <div className="mt-12 flex w-full max-w-4xl justify-center gap-8">
+          <div className="mt-4 flex w-full max-w-4xl justify-center">
             <div>
               <Image
                 placeholder="blur"
@@ -97,31 +97,59 @@ export default function Home() {
         )}
       </div>
 
-      <footer className="mt-16 w-full items-center pb-10 text-center text-gray-300 md:flex md:justify-between md:text-xs lg:text-sm">
-        <p>Powered by Together.ai & Flux</p>
+      <footer className="mt-16 w-full items-center pb-10 text-center text-gray-300 md:mt-4 md:flex md:justify-between md:pb-5 md:text-xs lg:text-sm">
+        <p>
+          Powered by{" "}
+          <a
+            href="https://www.dub.sh/together-ai"
+            target="_blank"
+            className="underline underline-offset-4 transition hover:text-blue-500"
+          >
+            Together.ai
+          </a>{" "}
+          &{" "}
+          <a
+            href="https://dub.sh/together-flux"
+            target="_blank"
+            className="underline underline-offset-4 transition hover:text-blue-500"
+          >
+            Flux
+          </a>
+        </p>
 
         <div className="mt-8 flex items-center justify-center md:mt-0 md:justify-between md:gap-6">
           <p className="hidden whitespace-nowrap md:block">
-            100% free and open source
+            100% free and{" "}
+            <a
+              href="https://github.com/Nutlope/blinkshot"
+              target="_blank"
+              className="underline underline-offset-4 transition hover:text-blue-500"
+            >
+              open source
+            </a>
           </p>
 
           <div className="flex gap-6 md:gap-2">
-            <Button
-              variant="outline"
-              size="sm"
-              className="inline-flex items-center gap-2"
-            >
-              <GithubIcon className="size-4" />
-              GitHub
-            </Button>
-            <Button
-              size="sm"
-              variant="outline"
-              className="inline-flex items-center gap-2"
-            >
-              <XIcon className="size-3" />
-              Twitter
-            </Button>
+            <a href="https://github.com/Nutlope/blinkshot" target="_blank">
+              <Button
+                variant="outline"
+                size="sm"
+                className="inline-flex items-center gap-2"
+              >
+                <GithubIcon className="size-4" />
+                GitHub
+              </Button>
+            </a>
+            <a href="https://x.com/nutlope" target="_blank">
+              <Button
+                size="sm"
+                variant="outline"
+                className="inline-flex items-center gap-2"
+              >
+                <XIcon className="size-3" />
+                Twitter
+              </Button>
+            </a>
           </div>
         </div>
       </footer>
